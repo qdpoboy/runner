247c63f27af83954c735af45d016ee922d79c86c
qdpoboy <280597526@qq.com>
2019-05-28 18:48:21 +0800

    jump game scene

---
+++ assets/scripts/Welcome.js
@@ -0,0 +1,21 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        
+    },
+
+    startGame: function() {
+        cc.director.loadScene("Game");
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start () {
+
+    },
+
+    // update (dt) {},
+});
